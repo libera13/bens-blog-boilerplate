9189deea472edaf39c4373575fcf5c57a3a48070
Emil <emillibera@gmail.com>
2021-04-28 12:38:58 +0200

    Updates

--- _nuxt/static/1616331460/blog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fbens-blog-boilerplate\u002F_nuxt\u002Fstatic\u002F1616331460",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{app:{basePath:"\u002Fbens-blog-boilerplate\u002F",assetsPath:"\u002Fbens-blog-boilerplate\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1619606335/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog\u002F2-incredible-tools","\u002Fuses","\u002Fblog\u002Fmy-first-post","\u002Fblog","\u002Fblog\u002Fmy-third-post","\u002Fblog\u002F4-react-query","\u002Fblog\u002F5-cool-functions"]})
